b6501bea1137ed7b74c56ed06c2bc0f736dd3095
= <=>
2023-12-16 22:05:55 -0300

    melhorias no arquivo de campaign_test

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,7 +1,9 @@
 package campaign
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestCampaign_NewCampaign(t *testing.T) {
@@ -19,11 +21,45 @@ func TestCampaign_NewCampaign(t *testing.T) {
 
 	campaign = *campaign.NewCampaign()
 
-	if campaign.ID != "1" {
-		t.Error("expected 1", campaign.ID)
+	assert.Equal(t, campaign.Name, "campaign X")
+}
+
+func TestCampaign_NewCampaign_IDIsNotNil(t *testing.T) {
+
+	name := "campaign X"
+	body := "Body Content"
+	contact := Contact{Email: "teste@gmail.com"}
+	contacts := []Contact{contact}
+
+	campaign := Campaign{
+		ID:      "1",
+		Name:    name,
+		Body:    body,
+		Contact: contacts,
 	}
 
-	if campaign.Name != name {
-		t.Error("expected ", name)
+	campaign = *campaign.NewCampaign()
+
+	assert.NotNil(t, campaign.ID, "valor não pode ser vazio")
+}
+
+func TestCampaign_NewCampaign_DateIsNotNil(t *testing.T) {
+
+	name := "campaign X"
+	body := "Body Content"
+	contact := Contact{Email: "teste@gmail.com"}
+	contacts := []Contact{contact}
+
+	campaign := Campaign{
+		ID:      "1",
+		Name:    name,
+		Body:    body,
+		Contact: contacts,
 	}
+
+	now := time.Now().Add(-time.Minute)
+
+	campaign = *campaign.NewCampaign()
+
+	assert.Greater(t, campaign.CreatedOn, now)
 }
